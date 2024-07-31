90fd5bacc6b27c04f5c0e56b444fffbadbabb06c
Sami Mussbach <sami.mussbach@posteo.de>
2024-07-31 11:31:47 +0200

    add tests

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// index.js
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! This is my PR 6');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+if (process.argv[1] === new URL(import.meta.url).pathname) {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+}
+
+export default app;
