d7585cd99f0f50ea387c63e72c382f433208fe52
Sami Mussbach <sami.mussbach@posteo.de>
2024-07-31 13:35:55 +0200

    fix test

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -3,13 +3,13 @@ import request from 'supertest';
 import app from '../index.js';
 
 describe('GET /', function () {
-    it('should return Hello World! This is my PR 6', function (done) {
+    it('should return Correct text', function (done) {
         request(app)
             .get('/')
             .end((err, res) => {
                 if (err) return done(err);
                 expect(res.status).to.equal(200);
-                expect(res.text).to.equal('Hello World! This is my PR 6');
+                expect(res.text).to.equal('Hello World! This is my PR 7');
                 done();
             });
     });
