c29a18fd7ef40e6b6576b4cda80fc4f0f1479adf
Sami Mussbach <sami.mussbach@posteo.de>
2024-07-31 11:15:10 +0200

    correct pr number

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! This is my PR 5');
+  res.send('Hello World! This is my PR 6');
 });
 
 app.listen(port, () => {
